a60d36f6bc700e90923de9fdc275de3697bf48e6
ZeroDav808 <199318346+ZeroDav808@users.noreply.github.com>
2025-09-08 15:22:00 -0500

    Added functionality to play 5 rounds of rock paper scissors

--- index.js
+++ index.js
@@ -1,1 +1,68 @@
-console.log('Hello, World!')
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice(){
+    const values = ['Rock', 'Paper', 'Scissors'];
+    return values[Math.floor(Math.random() * 3)];
+}
+
+function getHumanChoice(){
+    const choice = prompt('Rock, Paper, Scissors?', 'Rock');
+    return choice;
+}
+
+function playRound(humanChoice, computerChoice){
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+
+    console.log(`The computer chose ${computerChoice}, the user chose ${humanChoice}`);
+
+    switch(humanChoice){
+        case 'rock':
+            if(computerChoice === humanChoice) return;
+            else if(computerChoice == 'scissors') {
+                humanScore++;
+            } else {
+                computerScore++;
+            }
+            break;
+        case 'paper':
+            if(computerChoice === humanChoice) break;
+            else if(computerChoice === 'scissors') {
+                computerScore++;
+            } else{
+                humanScore++;
+            }
+            break;
+        case 'scissors':
+            if(computerChoice === humanChoice) break;
+            else if(computerChoice === 'rock'){
+                computerChoice++;
+            } else {
+                humanChoice++;
+            }
+            break;
+    }
+
+    console.log(`Current score: Human - ${humanScore}, Computer - ${computerScore}`);
+}
+
+function playGame() {
+    const ROUNDS = 5;
+
+    for(let i = 0; i < ROUNDS; i++){
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+
+        playRound(humanChoice, computerChoice);
+    }
+
+    if(humanScore > computerScore){
+        console.log('Human wins!')
+    } else{
+        console.log('Computer wins!');
+    }
+}
+
+
+playGame();
